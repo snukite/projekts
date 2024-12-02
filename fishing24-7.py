5fcaa2694e29943affa930ed66579d48eca36c7f
snukite <lauranuke12@gmail.com>
2024-12-02 12:37:03 +0200

    Add files via upload

---
+++ fishing24-7.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Informatīvā programma FISHING24-7 tirdzniecības automātiem")
+window.geometry("1150x700")
+window.resizable(False, False)
+window.configure(bg="#e6f0cd")
+
+window.mainloop()
