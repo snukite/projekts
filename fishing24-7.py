e2e4bf623fdb138ab3d0fac629bdac58feee5e7b
snukite <lauranuke12@gmail.com>
2024-12-02 13:40:51 +0200

    Update fishing24-7.py

--- fishing24-7.py
+++ fishing24-7.py
@@ -1,14 +1,22 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.title("Informatīvā programma FISHING24-7 tirdzniecības automātiem")
 window.geometry("1150x700")
 window.resizable(False, False)
-window.configure(bg="#e6f0cd")
+window.configure(bg="#c0d099")
 
-sakuma_skats = tk.Frame(window,bg="#e6f0cd")
+sakuma_skats = tk.Frame(window,bg="#c0d099")
 sakuma_skats.pack(fill='both', expand=True)
-uzraksts = tk.Label(sakuma_skats, text="FISHING24-7", font=("Verdana", 30, "bold"), fg="#b90843", bg="#e6f0cd")
+uzraksts = tk.Label(sakuma_skats, text="FISHING24-7", font=("Verdana", 30, "bold"), fg="#b90843", bg="#c0d099")
 uzraksts.pack(pady=30)
+
+original_image = Image.open("automats.png")
+resized_image = original_image.resize((270, 400)) 
+button_image = ImageTk.PhotoImage(resized_image)
+
+image_button = tk.Button(sakuma_skats, image=button_image, borderwidth=0, bg="#c0d099", activebackground="#c0d099", command=lambda: print("Poga nospiesta!"))
+image_button.pack(pady=50)
 
 window.mainloop()
