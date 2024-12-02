667741547a09f36fa165090add8f95114cb1a5ad
snukite <lauranuke12@gmail.com>
2024-12-02 13:08:46 +0200

    Update fishing24-7.py

--- fishing24-7.py
+++ fishing24-7.py
@@ -6,4 +6,9 @@ window.geometry("1150x700")
 window.resizable(False, False)
 window.configure(bg="#e6f0cd")
 
+sakuma_skats = tk.Frame(window,bg="#e6f0cd")
+sakuma_skats.pack(fill='both', expand=True)
+uzraksts = tk.Label(sakuma_skats, text="FISHING24-7", font=("Verdana", 30, "bold"), fg="#b90843", bg="#e6f0cd")
+uzraksts.pack(pady=30)
+
 window.mainloop()
