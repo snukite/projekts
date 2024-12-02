29cb5f6c5df224363ef1b686916e4f80d305ac4d
snukite <lauranuke12@gmail.com>
2024-12-02 14:02:22 +0200

    Update fishing24-7.py

--- fishing24-7.py
+++ fishing24-7.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+def paradit_tirdzniecibas_skatu():
+    sakuma_skats.pack_forget()  # Paslēpj sākuma skatu
+    tirdzniecibas_skats.pack(fill='both', expand=True)
+
 window = tk.Tk()
 window.title("Informatīvā programma FISHING24-7 tirdzniecības automātiem")
 window.geometry("1150x700")
@@ -16,10 +20,12 @@ original_image = Image.open("automats.png")
 resized_image = original_image.resize((270, 400)) 
 button_image = ImageTk.PhotoImage(resized_image)
 
-image_button = tk.Button(sakuma_skats, image=button_image, borderwidth=0, bg="#c0d099", activebackground="#c0d099", command=lambda: print("Poga nospiesta!"))
+image_button = tk.Button(sakuma_skats, image=button_image, borderwidth=0, bg="#c0d099", activebackground="#c0d099", command=paradit_tirdzniecibas_skatu)
 image_button.pack(pady=30)
 
 teksts_zem_pogas = tk.Label(sakuma_skats, text="Klikšķini uz tirdzniecības automāta, lai sāktu", font=("Verdana", 16), fg="#b90843", bg="#c0d099")
 teksts_zem_pogas.pack(pady=10)
+
+tirdzniecibas_skats = tk.Frame(window, bg="#c0d099")
 
 window.mainloop()
