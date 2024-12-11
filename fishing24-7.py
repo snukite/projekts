37d38a468e6eb7f6ddcc964e74daac8ce4394377
snukite <lauranuke12@gmail.com>
2024-12-11 10:01:22 +0200

    attēlus sagrupē mapē bildes

--- fishing24-7.py
+++ fishing24-7.py
@@ -19,7 +19,7 @@ def instrukcijas_poga():
     instrukcijas_logs.resizable(False, False)
     instrukcijas_logs.configure(bg="#c0d099")
 
-    instrukcijas_attels_path = "instrukcija.png" 
+    instrukcijas_attels_path = "bildes/instrukcija.png" 
     instrukcijas_image = Image.open(instrukcijas_attels_path)
     instrukcijas_photo = ImageTk.PhotoImage(instrukcijas_image)
 
@@ -37,7 +37,7 @@ def norekinasanas_poga():
     norekinasanas_logs.resizable(False, False)
     norekinasanas_logs.configure(bg="#c0d099")
 
-    norekinasanas_attels_path = "norekinasanas.png" 
+    norekinasanas_attels_path = "bildes/norekinasanas.png" 
     norekinasanas_image = Image.open(norekinasanas_attels_path)
     norekinasanas_photo = ImageTk.PhotoImage(norekinasanas_image)
 
@@ -63,7 +63,7 @@ sakuma_skats.pack(fill='both', expand=True)
 uzraksts = tk.Label(sakuma_skats, text="FISHING24-7", font=("Verdana", 30, "bold"), fg="#b90843", bg="#c0d099")
 uzraksts.pack(pady=20)
 
-original_image = Image.open("automats.png")
+original_image = Image.open("bildes/automats.png")
 resized_image = original_image.resize((324, 480)) 
 button_image = ImageTk.PhotoImage(resized_image)
 
@@ -82,7 +82,7 @@ bg="#c0d099",
 wraplength=600, justify="left", anchor="e")
 tirdzniecibas_teksts.pack(pady=50, padx=30, anchor="e")
 
-tirdzniecibas_attels_path = "plaukti.png"  
+tirdzniecibas_attels_path = "bildes/plaukti.png"  
 original_image = Image.open(tirdzniecibas_attels_path)
 resized_image = original_image.resize((488, 635))  
 tirdzniecibas_photo = ImageTk.PhotoImage(resized_image)
@@ -143,79 +143,79 @@ def izveidot_attelu_pogu3(skats, x, y, attela_nosaukums, komanda):
     poga.image = button_image  
     poga.place(x=x, y=y)
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 85, 75, "bigcarp_kuku.png", lambda: paradit_plaukta_saturu("bigcarp_kuku.png", "Big Carp Kukurūza", "4.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 85, 75, "bildes/bigcarp_kuku.png", lambda: paradit_plaukta_saturu("bildes/bigcarp_kuku.png", "Big Carp Kukurūza", "4.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 150, 75, "bigcarp_zem.png", lambda: paradit_plaukta_saturu("bigcarp_zem.png", "BigCarp Zemene 1kg", "4.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 150, 75, "bildes/bigcarp_zem.png", lambda: paradit_plaukta_saturu("bildes/bigcarp_zem.png", "BigCarp Zemene 1kg", "4.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 215, 75, "sekrbreksis.png", lambda: paradit_plaukta_saturu("sekrbreksis.png", "Sekr Breksis Melns 1kg", "3.90"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 215, 75, "bildes/sekrbreksis.png", lambda: paradit_plaukta_saturu("bildes/sekrbreksis.png", "Sekr Breksis Melns 1kg", "3.90"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 280, 75, "sekrlinkar.png", lambda: paradit_plaukta_saturu("sekrlinkar.png", "Sekr LīnKar Marc 1kg", "3.90"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 280, 75, "bildes/sekrlinkar.png", lambda: paradit_plaukta_saturu("bildes/sekrlinkar.png", "Sekr LīnKar Marc 1kg", "3.90"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 85, 134, "speclinkar.png", lambda: paradit_plaukta_saturu("speclinkar.png", "Spec LīnisKarūsa 1kg", "3.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 85, 134, "bildes/speclinkar.png", lambda: paradit_plaukta_saturu("bildes/speclinkar.png", "Spec LīnisKarūsa 1kg", "3.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 150, 134, "specuni.png", lambda: paradit_plaukta_saturu("specuni.png", "Spec Universālā 1kg", "3.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 150, 134, "bildes/specuni.png", lambda: paradit_plaukta_saturu("bildes/specuni.png", "Spec Universālā 1kg", "3.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 215, 134, "specfeeder.png", lambda: paradit_plaukta_saturu("specfeeder.png", "Spec Feeder 1kg", "3.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 215, 134, "bildes/specfeeder.png", lambda: paradit_plaukta_saturu("bildes/specfeeder.png", "Spec Feeder 1kg", "3.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 280, 134, "specbrek.png", lambda: paradit_plaukta_saturu("specbrek.png", "Spec Breksis 1kg", "3.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 280, 134, "bildes/specbrek.png", lambda: paradit_plaukta_saturu("bildes/specbrek.png", "Spec Breksis 1kg", "3.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 75, 205, "universala750.png", lambda: paradit_plaukta_saturu("universala750.png", "Universālā 750g", "2.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 75, 205, "bildes/universala750.png", lambda: paradit_plaukta_saturu("bildes/universala750.png", "Universālā 750g", "2.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 129, 205, "karpa750.png", lambda: paradit_plaukta_saturu("karpa750.png", "Karpa 750g", "2.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 129, 205, "bildes/karpa750.png", lambda: paradit_plaukta_saturu("bildes/karpa750.png", "Karpa 750g", "2.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 183, 205, "feeder750.png", lambda: paradit_plaukta_saturu("feeder750.png", "Feeder 750g", "2.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 183, 205, "bildes/feeder750.png", lambda: paradit_plaukta_saturu("bildes/feeder750.png", "Feeder 750g", "2.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 235, 205,  "linkar750.png", lambda: paradit_plaukta_saturu("linkar750.png", "Līnis-Karūsa 750g", "2.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 235, 205,  "bildes/linkar750.png", lambda: paradit_plaukta_saturu("bildes/linkar750.png", "Līnis-Karūsa 750g", "2.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 290, 205, "breksis750.png", lambda: paradit_plaukta_saturu("breksis750.png", "Breksis 750g", "2.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 290, 205, "bildes/breksis750.png", lambda: paradit_plaukta_saturu("bildes/breksis750.png", "Breksis 750g", "2.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 73, 266, "waftersource.png", lambda: paradit_plaukta_saturu("waftersource.png", " Vafteri Source 15mm", "6.00"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 73, 266, "bildes/waftersource.png", lambda: paradit_plaukta_saturu("bildes/waftersource.png", " Vafteri Source 15mm", "6.00"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 119, 266, "waftertignut.png", lambda: paradit_plaukta_saturu("waftertignut.png", "Vafteri TigNut 15mm", "7.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 119, 266, "bildes/waftertignut.png", lambda: paradit_plaukta_saturu("bildes/waftertignut.png", "Vafteri TigNut 15mm", "7.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 163, 266, "naktstarpi.png", lambda: paradit_plaukta_saturu("naktstarpi.png", "Nakstārpi", "4.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 163, 266, "bildes/naktstarpi.png", lambda: paradit_plaukta_saturu("bildes/naktstarpi.png", "Nakstārpi", "4.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 210, 266, "graudumix.png", lambda: paradit_plaukta_saturu("graudumix.png", "Graudu Mix 500g", "3.50"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 210, 266, "bildes/graudumix.png", lambda: paradit_plaukta_saturu("bildes/graudumix.png", "Graudu Mix 500g", "3.50"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 251, 266, "fluomix.png", lambda: paradit_plaukta_saturu("fluomix.png", "Piedeva FluoMix 400g", "4.90"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 251, 266, "bildes/fluomix.png", lambda: paradit_plaukta_saturu("bildes/fluomix.png", "Piedeva FluoMix 400g", "4.90"))
 
-izveidot_attelu_pogu(tirdzniecibas_skats, 294, 266, "fluozala.png", lambda: paradit_plaukta_saturu("fluozala.png", "Piedeva FluoZaļ 400g", "5.70"))
+izveidot_attelu_pogu(tirdzniecibas_skats, 294, 266, "bildes/fluozala.png", lambda: paradit_plaukta_saturu("bildes/fluozala.png", "Piedeva FluoZaļ 400g", "5.70"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 72, 326, "kukuruza.png", lambda: paradit_plaukta_saturu("kukuruza.png", "Kukurūza 212ml", "4.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 72, 326, "bildes/kukuruza.png", lambda: paradit_plaukta_saturu("bildes/kukuruza.png", "Kukurūza 212ml", "4.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 103, 326, "melase.png", lambda: paradit_plaukta_saturu("melase.png", "Melase Breksis 350g", "2.90"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 103, 326, "bildes/melase.png", lambda: paradit_plaukta_saturu("bildes/melase.png", "Melase Breksis 350g", "2.90"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 132, 326, "nr4.png", lambda: paradit_plaukta_saturu("nr4.png", "Sliekas Nr.4", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 132, 326, "bildes/nr4.png", lambda: paradit_plaukta_saturu("bildes/nr4.png", "Sliekas Nr.4", "2.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 161, 326, "nr4.png", lambda: paradit_plaukta_saturu("nr4.png", "Sliekas Nr.4", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 161, 326, "bildes/nr4.png", lambda: paradit_plaukta_saturu("bildes/nr4.png", "Sliekas Nr.4", "2.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 190, 326, "nr4.png", lambda: paradit_plaukta_saturu("nr4.png", "Sliekas Nr.4", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 190, 326, "bildes/nr4.png", lambda: paradit_plaukta_saturu("bildes/nr4.png", "Sliekas Nr.4", "2.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 220, 326, "nr3.png", lambda: paradit_plaukta_saturu("nr3.png", "Sliekas Nr.3", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 220, 326, "bildes/nr3.png", lambda: paradit_plaukta_saturu("bildes/nr3.png", "Sliekas Nr.3", "2.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 247, 326, "nr3.png", lambda: paradit_plaukta_saturu("nr3.png", "Sliekas Nr.3", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 247, 326, "bildes/nr3.png", lambda: paradit_plaukta_saturu("bildes/nr3.png", "Sliekas Nr.3", "2.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 278, 326, "nr3.png", lambda: paradit_plaukta_saturu("nr3.png", "Sliekas Nr.3", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 278, 326, "bildes/nr3.png", lambda: paradit_plaukta_saturu("bildes/nr3.png", "Sliekas Nr.3", "2.00"))
 
-izveidot_attelu_pogu2(tirdzniecibas_skats, 309, 326, "nr3.png", lambda: paradit_plaukta_saturu("nr3.png", "Sliekas Nr.3", "2.00"))
+izveidot_attelu_pogu2(tirdzniecibas_skats, 309, 326, "bildes/nr3.png", lambda: paradit_plaukta_saturu("bildes/nr3.png", "Sliekas Nr.3", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 72, 375, "baltie.png", lambda: paradit_plaukta_saturu("baltie.png", "Baltie tārpi 50g", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 72, 375, "bildes/baltie.png", lambda: paradit_plaukta_saturu("bildes/baltie.png", "Baltie tārpi 50g", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 103, 375, "baltie.png", lambda: paradit_plaukta_saturu("baltie.png", "Baltie tārpi 50g", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 103, 375, "bildes/baltie.png", lambda: paradit_plaukta_saturu("bildes/baltie.png", "Baltie tārpi 50g", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 132, 375, "baltie.png", lambda: paradit_plaukta_saturu("baltie.png", "Baltie tārpi 50g", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 132, 375, "bildes/baltie.png", lambda: paradit_plaukta_saturu("bildes/baltie.png", "Baltie tārpi 50g", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 161, 375, "baltie.png", lambda: paradit_plaukta_saturu("baltie.png", "Baltie tārpi 50g", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 161, 375, "bildes/baltie.png", lambda: paradit_plaukta_saturu("bildes/baltie.png", "Baltie tārpi 50g", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 190, 375, "baltie.png", lambda: paradit_plaukta_saturu("baltie.png", "Baltie tārpi 50g", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 190, 375, "bildes/baltie.png", lambda: paradit_plaukta_saturu("bildes/baltie.png", "Baltie tārpi 50g", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 220, 375, "pinki.png", lambda: paradit_plaukta_saturu("pinki.png", "Pinki 50g", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 220, 375, "bildes/pinki.png", lambda: paradit_plaukta_saturu("bildes/pinki.png", "Pinki 50g", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 247, 375, "nr2.png", lambda: paradit_plaukta_saturu("nr2.png", "Sliekas Nr.2", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 247, 375, "bildes/nr2.png", lambda: paradit_plaukta_saturu("bildes/nr2.png", "Sliekas Nr.2", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 278, 375, "nr2.png", lambda: paradit_plaukta_saturu("nr2.png", "Sliekas Nr.2", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 278, 375, "bildes/nr2.png", lambda: paradit_plaukta_saturu("bildes/nr2.png", "Sliekas Nr.2", "2.00"))
 
-izveidot_attelu_pogu3(tirdzniecibas_skats, 309, 375, "nr2.png", lambda: paradit_plaukta_saturu("nr2.png", "Sliekas Nr.2", "2.00"))
+izveidot_attelu_pogu3(tirdzniecibas_skats, 309, 375, "bildes/nr2.png", lambda: paradit_plaukta_saturu("bildes/nr2.png", "Sliekas Nr.2", "2.00"))
 
 lokacijas_poga = tk.Button(tirdzniecibas_skats, text="FISHING24-7 tirdzniecības automātu lokācijas", font=("Verdana", 14), fg="#b90843", bg="white", borderwidth=0, command=lokacijas_poga)
 lokacijas_poga.pack(pady=(30), anchor="e", padx=100) 
@@ -238,7 +238,7 @@ bg="#c0d099",
 wraplength=500, justify="left", anchor="w")
 lokacijas_teksts1.grid(row=1, column=0, padx=30, pady=1, sticky="w")
 
-image_path = "sigulda.png" 
+image_path = "bildes/sigulda.png" 
 image = Image.open(image_path).resize((400, 135))  
 photo = ImageTk.PhotoImage(image)
 lokacijas_attels = tk.Label(lokacijas_skats, image=photo, bg="#c0d099")
@@ -251,7 +251,7 @@ bg="#c0d099",
 wraplength=500, justify="left", anchor="w")
 lokacijas_teksts2.grid(row=3, column=0, padx=30, pady=1, sticky="w")
 
-image_path2 = "brivibas.png" 
+image_path2 = "bildes/brivibas.png" 
 image2 = Image.open(image_path2).resize((400, 135))  
 photo2 = ImageTk.PhotoImage(image2)
 lokacijas_attels2 = tk.Label(lokacijas_skats, image=photo2, bg="#c0d099")
@@ -264,7 +264,7 @@ bg="#c0d099",
 wraplength=500, justify="left", anchor="w")
 lokacijas_teksts3.grid(row=5, column=0, padx=30, pady=10, sticky="w")
 
-image_path3 = "vaidavas.png" 
+image_path3 = "bildes/vaidavas.png" 
 image3 = Image.open(image_path3).resize((400, 135))  
 photo3 = ImageTk.PhotoImage(image3)
 lokacijas_attels3 = tk.Label(lokacijas_skats, image=photo3, bg="#c0d099")
@@ -277,7 +277,7 @@ bg="#c0d099",
 wraplength=500, justify="left", anchor="w")
 lokacijas_teksts4.grid(row=1, column=1, padx=30, pady=10, sticky="w")
 
-image_path4 = "latgales.png" 
+image_path4 = "bildes/latgales.png" 
 image4 = Image.open(image_path4).resize((400, 135))  
 photo4 = ImageTk.PhotoImage(image4)
 lokacijas_attels4 = tk.Label(lokacijas_skats, image=photo4, bg="#c0d099")
@@ -290,7 +290,7 @@ bg="#c0d099",
 wraplength=500, justify="left", anchor="w")
 lokacijas_teksts5.grid(row=3, column=1, padx=30, pady=10, sticky="w")
 
-image_path5 = "kalnciema.png" 
+image_path5 = "bildes/kalnciema.png" 
 image5 = Image.open(image_path5).resize((400, 135))  
 photo5 = ImageTk.PhotoImage(image5)
 lokacijas_attels5 = tk.Label(lokacijas_skats, image=photo5, bg="#c0d099")
